626d740edd669a07ba60d1c1e348b9937dac29fd
Aditi Sarode <aditisarode1234@gmail.com>
2024-10-02 15:36:30 +0530

    Navigation done

---
+++ src/customer/components/Navigation/navigationData.js
@@ -0,0 +1,127 @@
+export const navigation = {
+    categories: [
+      {
+        id: 'women',
+        name: 'Women',
+        featured: [
+          {
+            name: 'New Arrivals',
+            id: '#',
+            imageSrc: "https://rukminim1.flixcart.com/image/612/612/kflftzk0/top/q/t/m/l-aw-2081w-aelomart-original-imafwyzhw8t2npsk.jpeg?q=70",
+            imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+          },
+          {
+            name: 'Artwork Tees',
+            id: '#',
+            imageSrc: "https://rukminim1.flixcart.com/image/612/612/l1fc0i80/lehenga-choli/j/k/t/free-half-sleeve-modak-rama-gajri-dishwa-fashion-original-imagczmhzqmzpyy9.jpeg?q=70",
+            imageAlt:
+              'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+          },
+        ],
+             
+          
+        sections: [
+          {
+            id: 'clothing',
+            name: 'Clothing',
+            items: [
+              { name: 'Tops', id:"top", href: `{women/clothing/tops}` },
+              { name: 'Dresses', id:"women_dress", href: '#' },
+              { name: 'Women Jeans', id: 'women_jeans' },
+              { name: 'Lengha Choli', id: 'lengha_choli' },
+              { name: 'Sweaters', id: 'sweater' },
+              { name: 'T-Shirts', id: 't-shirt' },
+              { name: 'Jackets', id: 'jacket' },
+              { name: 'Gouns', id: 'gouns' },
+              { name: 'Sarees', id: 'saree' },
+              { name: 'Kurtas', id: 'kurtas' },
+            ],
+          },
+          {
+            id: 'accessories',
+            name: 'Accessories',
+            items: [
+              { name: 'Watches', id: 'watch' },
+              { name: 'Wallets', id: 'wallet' },
+              { name: 'Bags', id: 'bag' },
+              { name: 'Sunglasses', id: 'sunglasse' },
+              { name: 'Hats', id: 'hat' },
+              { name: 'Belts', id: 'belt' },
+            ],
+          },
+          {
+            id: 'brands',
+            name: 'Brands',
+            items: [
+              { name: 'Full Nelson', id: '#' },
+              { name: 'My Way', id: '#' },
+              { name: 'Re-Arranged', id: '#' },
+              { name: 'Counterfeit', id: '#' },
+              { name: 'Significant Other', id: '#' },
+            ],
+          },
+        ],
+      },
+      {
+        id: 'men',
+        name: 'Men',
+        featured: [
+          {
+            name: 'New Arrivals',
+            id: '#',
+            imageSrc: "https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/i/v/x/xxl-br-ad-kt-105-adwyn-peter-original-imagj4zyd2q7t6cg.jpeg?q=70",
+            imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+          },
+          {
+            name: 'Artwork Tees',
+            id: '#',
+            imageSrc: "https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/f/z/l/m-white-formal-youthfirst-original-imagqjdfgwjfwgsb.jpeg?q=70",
+            imageAlt:
+              'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+          },
+        ],
+        sections: [
+          {
+            id: 'clothing',
+            name: 'Clothing',
+            items: [
+              { name: 'Mens Kurtas', id: 'mens_kurta' },
+              { name: 'Shirt', id: 'shirt' },
+              { name: 'Men Jeans', id: 'men_jeans' },
+              { name: 'Sweaters', id: '#' },
+              { name: 'T-Shirts', id: 't-shirt' },
+              { name: 'Jackets', id: '#' },
+              { name: 'Activewear', id: '#' },
+              
+            ],
+          },
+          {
+            id: 'accessories',
+            name: 'Accessories',
+            items: [
+              { name: 'Watches', id: '#' },
+              { name: 'Wallets', id: '#' },
+              { name: 'Bags', id: '#' },
+              { name: 'Sunglasses', id: '#' },
+              { name: 'Hats', id: '#' },
+              { name: 'Belts', id: '#' },
+            ],
+          },
+          {
+            id: 'brands',
+            name: 'Brands',
+            items: [
+              { name: 'Re-Arranged', id: '#' },
+              { name: 'Counterfeit', id: '#' },
+              { name: 'Full Nelson', id: '#' },
+              { name: 'My Way', id: '#' },
+            ],
+          },
+        ],
+      },
+    ],
+    pages: [
+      { name: 'Company', id: '/' },
+      { name: 'Stores', id: '/' },
+    ],
+  }
